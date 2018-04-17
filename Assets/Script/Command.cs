d17ce31fdb59440e1a8d87df2199368f148e5a25
Paul <cyberprime99@gmail.com>
2018-04-17 14:34:09 +0200

    First push: You are Welcome!

---
+++ Assets/Script/Command.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Command : MonoBehaviour {
+
+	delegate void CommandMethod();
+	List<CommandMethod> commandList = new List<CommandMethod>();
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	void CreateListCmd()
+	{
+		
+		commandList.Add(move);
+		commandList.Add(rotate);
+		
+	}
+	void execute()
+	{
+		commandList[0]();
+	}
+	
+	void move()
+	{
+		//Instantiate something;
+	}
+	
+	void rotate()
+	{
+		//Instantiate something;
+	}
+	
+	void action()
+	{
+		
+	}
+	
+	void jump()
+	{
+		
+	}
+	
+}
