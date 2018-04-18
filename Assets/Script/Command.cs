7a833708cf855905e5547c81559dbdbca2440986
Paul <cyberprime99@gmail.com>
2018-04-18 10:08:16 +0200

    Added Some Commands

--- Assets/Script/Command.cs
+++ Assets/Script/Command.cs
@@ -7,15 +7,35 @@ public class Command : MonoBehaviour {
 
 	delegate void CommandMethod();
 	List<CommandMethod> commandList = new List<CommandMethod>();
+	public GameObject player = null;
 	
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		var y=0f;
+        var z=0f;
+		if(Input.GetKeyDown("up")||Input.GetKeyDown("down"))
+		{
+			if(Input.GetKeyDown("up"))
+			z = 12.0f;
+			else
+				z = -12.0f;
+	
+		}
+		if(Input.GetKeyDown("right")||Input.GetKeyDown("left"))
+		{
+			if(Input.GetKeyDown("right"))
+			y = 90.0f;
+			else
+				y = -90.0f;
+		}
 		
+        player.transform.Translate(0, 0, z);
+		player.transform.Rotate(0, y, 0);
 	}
 	
 	
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 		
