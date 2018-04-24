a5aefef27f5029b2cac2600483a59f7fbbfa4155
silveryght <karim.grandemange@ynov.com>
2018-04-24 12:18:44 +0200

    drag on drop

---
+++ Assets/Script/DragHandeler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+    #region IBeginDragHandler implementation
+
+    public void OnBeginDrag (PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    #endregion
+
+    //mouse input
+
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+    #endregion
+
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent != startParent)
+        {
+            transform.position = startPosition;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Script/Slor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slor : MonoBehaviour, IDropHandler {
+
+	public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    #region IDropHandler implementation
+    public void OnDrop (PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandeler.itemBeingDragged.transform.SetParent(transform);
+            
+        }
+    }
+    #endregion
+}
