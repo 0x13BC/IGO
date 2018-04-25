673876f07ef9d48b13b3e4bc3c2dc9c30b021599
Paul <cyberprime99@gmail.com>
2018-04-25 16:53:59 +0200

    Game with Menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+	}
+	
+	// Update is called once per frame
+	public void Quit () {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
