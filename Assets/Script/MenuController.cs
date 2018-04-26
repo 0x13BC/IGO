3a7deefbcad6f68a7577aedeadb057216f532b34
silveryght <karim.grandemange@ynov.com>
2018-04-26 09:58:51 +0200

    Pause en cour

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public string mainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+	}
+
+    public void ResumeGame()
+    {
+
+    }
+
+    
+}
